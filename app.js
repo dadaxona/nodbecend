1d6be29be95c8a905c41a5664d9f70a5673e97e0
dadaxona <abdullaevdadaxon337@gmail.com>
2025-09-09 11:51:40 +0500

    a

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api', route)
 
 app.listen(process.env.PORT || 1122)
+
+// npx sequelize-cli model:generate --name User --attributes firstName:string,lastName:string,email:string
+// npx sequelize-cli db:migrate
+// npx sequelize-cli db:migrate:undo:all
